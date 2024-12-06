60742aaba98d1b3103f7825d8451aece3e3a46c4
ueki3 <k-ueki@rinjinsystem.com>
2024-12-06 23:19:04 +0900

    Pagination の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { 
-  useReactTable, 
-  getCoreRowModel,
+import {
   flexRender,
-  createColumnHelper
-} from '@tanstack/react-table';
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+} from "@tanstack/react-table";
+import { useEffect, useState } from "react";
 
 type Post = {
   userId: number;
@@ -15,11 +15,12 @@ type Post = {
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [pageSize, setPageSize] = useState<number>(30);
 
   useEffect(() => {
     const getPosts = async () => {
       const response = await fetch(
-        'https://jsonplaceholder.typicode.com/posts'
+        "https://jsonplaceholder.typicode.com/posts"
       );
       const data = await response.json();
       setPosts(data);
@@ -28,52 +29,90 @@ function App() {
     getPosts();
   }, []);
 
-  const updateRow = (id: number) => {
-    console.log('update', id);
-  };
-  
-  const deleteRow = (id: number) => {
-    console.log('delete', id);
-  };
-  
-  const columnHelper = createColumnHelper<Post>();
-  
   const columns = [
-    columnHelper.accessor((row) => `${row.userId} ${row.id}`, {
-      id: 'WID',
-    }),
-    columnHelper.accessor('title', {
-      header: 'Title',
-      cell: (props) => props.getValue().toUpperCase(),
-    }),
-    columnHelper.accessor('body', {
-      header: () => 'Body',
-    }),
-    columnHelper.display({
-      id: 'update',
-      header: '更新',
-      cell: (props) => (
-        <button onClick={() => updateRow(props.row.original.id)}>更新</button>
-      ),
-    }),
-    columnHelper.display({
-      id: 'delete',
-      header: () => '削除',
-      cell: (props) => (
-        <button onClick={() => deleteRow(props.row.original.id)}>削除</button>
-      ),
-    }),
+    {
+      accessorKey: "userId",
+      header: "User ID",
+    },
+    {
+      accessorKey: "id",
+      header: "ID",
+    },
+    {
+      accessorKey: "title",
+      header: "Title",
+    },
+    {
+      accessorKey: "body",
+      header: "Body",
+    },
   ];
 
   const table = useReactTable({
     data: posts,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 30,
+      },
+    },
   });
 
   return (
-    <div style={{ margin: '2em' }}>
+    <div style={{ margin: "2em" }}>
       <h1>Posts List</h1>
+      <div style={{ marginBottom: "1em" }}>
+        <div>Page Size</div>
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            table.setPageSize(parseInt(e.target.value));
+            setPageSize(parseInt(e.target.value));
+          }}
+        >
+          <option value={10}>10</option>
+          <option value={30}>30</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+        </select>
+      </div>
+      <div style={{ display: "flex", marginBottom: "1em" }}>
+        <button
+          disabled={!table.getCanPreviousPage()}
+          onClick={() => table.previousPage()}
+        >
+          Privious
+        </button>
+        {Array.from({ length: table.getPageCount() }, (_, i) => i).map(
+          (index) => (
+            <div
+              key={index}
+              style={{
+                backgroundColor:
+                  table.getState().pagination.pageIndex === index ? "blue" : "",
+                color:
+                  table.getState().pagination.pageIndex === index
+                    ? "white"
+                    : "black",
+                padding: "0 0.5em 0 0.5em",
+                margin: "0 0.2em 0 0.2em",
+                cursor: "pointer",
+              }}
+              onClick={() => table.setPageIndex(index)}
+            >
+              {index + 1}
+            </div>
+          )
+        )}
+        <button
+          disabled={!table.getCanNextPage()}
+          onClick={() => table.nextPage()}
+        >
+          Next
+        </button>
+      </div>
       <table>
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -90,10 +129,7 @@ function App() {
           ))}
         </thead>
         <tbody>
-        {table
-          .getRowModel()
-          .rows.slice(0, 5)
-          .map((row) => (
+          {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
                 <td key={cell.id}>
@@ -104,7 +140,6 @@ function App() {
           ))}
         </tbody>
       </table>
-      <p>Rows Number: {table.getRowModel().rows.length}</p>
     </div>
   );
 }
